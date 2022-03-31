3d580eb57c630ec7d91a8488153fddf75e74bcb7
kurochkinpa <kurochkin.ps@phystech.edu>
2022-03-31 21:00:46 +0300

    readme

--- run.sh
+++ run.sh
@@ -3,4 +3,12 @@ for number in {1..19}; do
 		echo -n "$number.png" > .input
 		python3 main.py
 	fi
+	if [ -f "./samples/$number.jpg" ]; then
+                echo -n "$number.jpg" > .input
+                python3 main.py
+	fi
+        if [ -f "./samples/$number.jpeg" ]; then
+                echo -n "$number.jpeg" > .input
+                python3 main.py
+	fi
 	done
